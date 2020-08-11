f5d331507c6160d87b22a47803f50393b691c197
majosue <majosue@iMac.local>
2020-08-11 16:02:52 +0300

    ft_lstp2back in printf fixed

--- libftprintf/ft_lstp2back.c
+++ libftprintf/ft_lstp2back.c
@@ -6,32 +6,40 @@
 /*   By: majosue <majosue@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 14:01:24 by majosue           #+#    #+#             */
-/*   Updated: 2020/08/11 14:38:11 by majosue          ###   ########.fr       */
+/*   Updated: 2020/05/03 18:19:25 by majosue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "lem-in.h"
 
-int	ft_lstp2back(t_list **begin_list, void const *content, size_t content_size)
+t_list *ft_lstp2back(t_list **begin_list, void const *content, size_t content_size)
 {
 	t_list *var1;
 
 	var1 = *begin_list;
 	if (var1 == 0)
 	{
-		*begin_list = ft_lstnew(content, content_size);
-		if (!*begin_list)
-			return (0);
+		return (*begin_list = ft_lstnew(content, content_size));
 	}
-	else
+	while (var1->next != 0)
 	{
-		while (var1->next != 0)
-		{
-			var1 = var1->next;
-		}
-		var1->next = ft_lstnew(content, content_size);
-		if (!var1->next)
-			return (0);
+		var1 = var1->next;
 	}
-	return (1);
+	var1->next = ft_lstnew(content, content_size);
+	return (var1->next);
+}
+
+t_list *ft_dequeue(t_list **queue)
+{
+	t_list *head;
+	void *content;
+
+	if (*queue == NULL)
+		return (NULL);
+	head = *queue;
+	*queue = (*queue)->next;
+	head->next = NULL;
+	content = *(t_list **)(head->content);
+	ft_lstdelone(&head, del);
+	return (content);
 }
--- libftprintf/ft_printf.h
+++ libftprintf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: majosue <majosue@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 20:17:59 by majosue           #+#    #+#             */
-/*   Updated: 2019/12/17 13:05:58 by majosue          ###   ########.fr       */
+/*   Updated: 2020/08/11 16:02:27 by majosue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ typedef union	u_type_u {
 
 int				ft_printf(const char *restrict format, ...);
 int				ft_chkflags(char **str, char *ftab[5]);
-int				ft_lstp2back(t_list **begin_list, void const *content,\
+t_list			*ft_lstp2back(t_list **begin_list, void const *content,\
 size_t content_size);
 int				ft_readformat(t_list **str, char *format);
 t_fun			ft_get_f(char c);
